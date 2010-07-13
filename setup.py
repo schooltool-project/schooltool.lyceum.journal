c4fd817033ca231d530a5829b31216ffbf59a71d
Gediminas Paulauskas <menesis@pov.lt>
2010-07-13 21:48:15 +0300

    Release 0.7.0

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     package_dir={'': 'src'},
     namespace_packages=["schooltool", "schooltool.lyceum"],
     packages=find_packages('src'),
-    install_requires=['schooltool>=1.5.0dev',
+    install_requires=['schooltool>=1.5.0',
                       'pytz',
                       'setuptools',
                       'zc.table',

051f60fe1ec88e7d4ab096b10cdf9f534d325e93
Justas Sadzevicius <justas@pov.lt>
2013-09-18 17:39:57 +0300

    Register LP project

--- src/schooltool/lyceum/journal/__init__.py
+++ src/schooltool/lyceum/journal/__init__.py
@@ -1,3 +1,7 @@
 #
 from zope.i18nmessageid import MessageFactory
 LyceumMessage = MessageFactory("schooltool.lyceum.journal")
+
+import schooltool.common
+
+schooltool.common.register_lauchpad_project(__package__, 'schooltool.lyceum.journal')
